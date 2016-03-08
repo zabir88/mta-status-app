f9fab83687e51894dff875b2d7053f7312101947
Zabir <zabir8809@gmail.com>
2016-03-08 07:33:52 -0500

    Change in display

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -4,13 +4,13 @@ class StatusesController < ApplicationController
 		require 'nokogiri'
 		data= Nokogiri::HTML(open("http://web.mta.info/status/serviceStatus.txt"))
 
-		@service_types = params[:service_types]
+		service_types = params[:service_types]
 		@service_types_capital= params[:service_types].to_s.upcase
 		@line_name, @status, @text, @timestamp= [], [], [], ''
 	
 		@timestamp<< data.at_css("service/timestamp")
 			
-		data.css("#{@service_types}/line").each do |categories|
+		data.css("#{service_types}/line").each do |categories|
 			@line_name<<categories.at_css('name').text
 			@status<<categories.at_css('status').text
 			@text<<categories.at_css('text').text.gsub(/<\/?[^>]*>/, '').gsub(/\n\n+/, "\n").gsub(/^\n|\n$/, '').gsub("&nbsp;", '').gsub( '&#149', '')
